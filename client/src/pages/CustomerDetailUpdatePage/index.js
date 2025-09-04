6e33cfe465bf42c0484278437b9eedc91fa43191
Peterson Winny Babu <88373271+PetersonWinnyBabu@users.noreply.github.com>
2025-09-05 02:51:45 +0530

    Update index.js

--- client/src/pages/CustomerDetailUpdatePage/index.js
+++ client/src/pages/CustomerDetailUpdatePage/index.js
@@ -31,7 +31,7 @@ const CustomerDetailUpdatePage = () => {
             alert("please Fill the Empty Fields")
         }
         else{
-            axios.put(`https://api-project-customer-management.onrender.com/api/customers/${id}`,body)
+            axios.put(`https://project-customer-management.onrender.com/api/customers/${id}`,body)
                 .then(response => {
                     alert(`${response.data.message}`);
                     navigate(-1)
@@ -94,4 +94,5 @@ const onBlurPhoneNumber = () => {
 
 
 export default CustomerDetailUpdatePage
+
 
