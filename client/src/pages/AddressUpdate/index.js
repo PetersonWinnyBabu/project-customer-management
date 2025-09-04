de94141902df3775ccdf42618d3ff0d2d2eb0093
Peterson Winny Babu <88373271+PetersonWinnyBabu@users.noreply.github.com>
2025-09-04 21:56:11 +0530

    Update index.js

--- client/src/pages/AddressUpdate/index.js
+++ client/src/pages/AddressUpdate/index.js
@@ -23,7 +23,7 @@ const  onFormSubmit = (event) => {
     console.log(addressDetails)
     console.log(city)
         // Fetch customers from the backend API
-    axios.put(`http://api-project-customer-management.onrender.com/api/addresses/${addressId}`,body)
+    axios.put(`https://api-project-customer-management.onrender.com/api/addresses/${addressId}`,body)
         .then(response => {
             alert(response.data.message);
             navigate(-1)
@@ -73,3 +73,4 @@ const  onFormSubmit = (event) => {
 
 
 export default AddressUpdatePage
+
