15708c65888dce07db4e69b3459a5852cc477966
Peterson Winny Babu <88373271+PetersonWinnyBabu@users.noreply.github.com>
2025-09-04 21:43:05 +0530

    Update index.js

--- client/src/components/AddressForm/index.js
+++ client/src/components/AddressForm/index.js
@@ -27,7 +27,7 @@ const  OnFormSubmit = (event) => {
     console.log(addressDetails)
     console.log(city)
         // Fetch customers from the backend API
-    axios.post(`http://localhost:5000/api/customers/${id}/addresses`,body)
+    axios.post(`http://https://api-project-customer-management.onrender.com/api/customers/${id}/addresses`,body)
         .then(response => {
             setresponseMessage(response.data.message);
             navigate(-1)
@@ -66,5 +66,6 @@ const  OnFormSubmit = (event) => {
  
     )
 }
+
 
 export default AddressForm
